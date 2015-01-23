3afe1e4bdd1441026fedb0450c11ae0f465eb66e
Noobay <shadowswatcher@gmail.com>
2015-01-23 12:45:22 +0200

    Added Main UI

---
+++ GGJ_PaperPark/Assets/DrawPanel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Draw : MonoBehaviour {
+
+	public float draw_distance;
+
+	void Update()
+	{
+	}
+
+	void DrawDrawer()
+	{
+		this.transform.position = Mathf.Lerp(t
+	}
+}
